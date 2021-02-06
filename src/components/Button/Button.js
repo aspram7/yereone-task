8da917aec2cede32ef673a31c695ba5e0c5eaaa3
Aspram <aspram.markosyan7@gmail.com>
2021-02-07 00:08:33 +0400

    Initial commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Button.scss";
+
+const Button = () => {
+  return <button className="app-button"></button>;
+};
+
+export default Button;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Layout.scss";
+
+const Layout = () => {
+  return <div className="app-layout"></div>;
+};
+
+export default Layout;
